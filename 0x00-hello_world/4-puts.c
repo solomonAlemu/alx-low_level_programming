5cc19fb2d16e59b2bf33fad795bff1c1dec19983
solomonAlemu <solomonakb@gmail.com>
2022-09-08 17:24:33 -0700

    exercise 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: Prints "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+	char string [] = "Programming is like building a multilingual puzzle";
+	puts(string);
+	return 0;
+}
