fc529f4c1207c04046d30d39973d9ee05913c7aa
solomonAlemu <solomonakb@gmail.com>
2022-09-11 02:14:18 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - betty style doc for function main goes there
+ *description: xxx
+ *Return: xxx
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if (n < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	return (0);
+}
