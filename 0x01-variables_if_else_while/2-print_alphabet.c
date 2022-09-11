d6483d6e487607f2eb108a7786cefa4018d7b696
solomonAlemu <solomonakb@gmail.com>
2022-09-11 05:47:26 -0700

    print lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ *main - Write a program that prints the alphabet in lowercase,
+*followed by a new line*
+ *description: xxx
+ *Return: xx
+ */
+int main ( void )
+{
+	char output[] = "abcdefghijklmnopqrstuvwxyz";
+
+	size_t i = 0;
+
+	size_t arrayLength = strlen( output );
+
+	for ( i = 0; i < arrayLength; i++ )
+	{
+		putchar(output[i]);
+	}
+	putchar( '\n');
+	return (0);
+}
