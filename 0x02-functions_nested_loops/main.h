5891f7fdfac4bcdc595a6de88f8e67a5299b0c40
solomonAlemu <solomonakb@gmail.com>
2022-09-13 10:05:18 -0700

    prints 10 times the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,4 +17,11 @@ int _putchar(char c);
  */
 void print_alphabet(void);
 
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed
+ * by  a new line.
+ *Return: void
+ *
+ */
+void print_alphabet_x10(void);
 #endif
